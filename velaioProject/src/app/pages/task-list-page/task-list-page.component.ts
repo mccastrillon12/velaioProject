e1eaa07752993d45b1a67efd0d6a13ed3535b212
camila <castrillonosorio12@gmail.com>
2024-09-28 18:20:51 -0500

    modicated take-list-page.component.html and task-list-page.component.ts

--- velaioProject/src/app/pages/task-list-page/task-list-page.component.ts
+++ velaioProject/src/app/pages/task-list-page/task-list-page.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TaskListComponent } from '../../components/task-list/task-list.component';
+import { TaskService } from '../../service/task.service';
 
 @Component({
   selector: 'app-task-list-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, TaskListComponent],
   templateUrl: './task-list-page.component.html',
-  styleUrl: './task-list-page.component.css'
+  styleUrls: ['./task-list-page.component.css']
 })
 export class TaskListPageComponent {
-
+  private taskService = inject(TaskService);
+  tasks$ = this.taskService.tasks$;
 }
