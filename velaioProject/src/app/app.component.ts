b532d6e27ddc8e1f2c87cecaa8f34fd04887ae6b
camila <castrillonosorio12@gmail.com>
2024-09-28 16:24:53 -0500

    created person-form component and logic for addSkill and removeSkill and html

--- velaioProject/src/app/app.component.ts
+++ velaioProject/src/app/app.component.ts
@@ -1,13 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TaskFormComponent } from './components/task-form/task-form/task-form.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule, TaskFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'velaioProject';
 }
+
+
