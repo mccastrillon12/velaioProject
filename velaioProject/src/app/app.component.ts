9eff4d96095336fb9403d4299a4cff62953421de
camila <castrillonosorio12@gmail.com>
2024-09-28 16:53:06 -0500

    created tas-list component and logic for markAsCompleted

--- velaioProject/src/app/app.component.ts
+++ velaioProject/src/app/app.component.ts
@@ -2,12 +2,13 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TaskFormComponent } from './components/task-form/task-form/task-form.component';
+import { TaskListComponent } from './components/task-list/task-list/task-list.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,CommonModule, TaskFormComponent],
+  imports: [RouterOutlet,CommonModule, TaskFormComponent, TaskListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
