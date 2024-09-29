6fea4aab099bb667bd0940e6d971e6e532d149ee
camila <castrillonosorio12@gmail.com>
2024-09-28 20:11:38 -0500

    added filter component for filtering tasks

---
+++ velaioProject/src/app/components/task-filter/task-filter/task-filter.component.ts
@@ -0,0 +1,19 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-task-filter',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './task-filter.component.html',
+  styleUrls: ['./task-filter.component.css'],
+})
+export class TaskFilterComponent {
+
+  @Output() filterChanged = new EventEmitter<string>();
+
+
+  setFilter(filter: string) {
+    this.filterChanged.emit(filter);
+  }
+}
--- velaioProject/src/app/pages/task-list-page/task-list-page.component.ts
+++ velaioProject/src/app/pages/task-list-page/task-list-page.component.ts
@@ -1,17 +1,43 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { TaskListComponent } from '../../components/task-list/task-list.component';
+import { ITask } from '../../models/task.model';
+import { TaskFilterComponent } from '../../components/task-filter/task-filter/task-filter.component';
 import { TaskService } from '../../service/task.service';
 
 @Component({
   selector: 'app-task-list-page',
   standalone: true,
-  imports: [CommonModule, TaskListComponent],
+  imports: [CommonModule, TaskListComponent, TaskFilterComponent],
   templateUrl: './task-list-page.component.html',
-  styleUrls: ['./task-list-page.component.css']
+  styleUrls: ['./task-list-page.component.css'],
 })
 export class TaskListPageComponent {
   private taskService = inject(TaskService);
   tasks$ = this.taskService.tasks$;
+  filteredTasks: ITask[] = [];
+
+  constructor() {
+    // Inicializar las tareas filtradas con todas las tareas
+    this.tasks$.subscribe((tasks) => {
+      this.filteredTasks = tasks;
+    });
+  }
+
+  // Método que recibe el valor del filtro y actualiza la lista de tareas
+  onFilterChanged(filter: string) {
+    this.tasks$.subscribe((tasks) => {
+      switch (filter) {
+        case 'completed':
+          this.filteredTasks = tasks.filter((task) => task.isCompleted);
+          break;
+        case 'pending':
+          this.filteredTasks = tasks.filter((task) => !task.isCompleted);
+          break;
+        default:
+          this.filteredTasks = tasks;
+          break;
+      }
+    });
+  }
 }
