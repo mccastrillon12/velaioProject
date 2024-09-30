080d319a7385a80b3a721fc7b25737dc6ec92967
camila <castrillonosorio12@gmail.com>
2024-09-29 19:52:42 -0500

    edited saveTask in task-form.component and unify logic for use only one component

--- velaioProject/src/app/app.routes.ts
+++ velaioProject/src/app/app.routes.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
-import { TaskFormComponent } from './components/task-form/task-form.component';
 import { TaskListPageComponent } from './pages/task-list-page/task-list-page.component';
 import { EditTaskPageComponent } from './pages/edit-task-page/edit-task-page.component';
+import { TaskCreatePageComponent } from './pages/task-create/task-create-page.component';
 
 export const routes: Routes = [
   { path: '', component: HomePageComponent },
-  { path: 'create-task', component: TaskFormComponent },
+  { path: 'create-task', component: TaskCreatePageComponent  },
   { path: 'all-tasks', component: TaskListPageComponent },
   { path: 'edit-task/:id', component: EditTaskPageComponent },
 ];
